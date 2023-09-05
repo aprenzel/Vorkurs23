e47006951132ef79e3d960139beffe36171c8333
aprenzel <aprenzel@hszg.de>
2023-09-05 13:57:01 +0200

    Vorkurs Vorbereitung

---
+++ Lernlisten 4/data.js
@@ -0,0 +1,4 @@
+var list_title = "Vogelarten in Europa";
+
+var list_card1_img = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Robin_%289509442456%29.jpg/512px-Robin_%289509442456%29.jpg";
+var list_card1_text = "Das Rotkehlchen (Erithacus rubecula) ist eine Vogelart aus der Familie der Fliegenschnäpper (Muscicapidae). Es besiedelt Nordafrika, Europa und Kleinasien sowie die Mittelmeerinseln. Seine Nahrung besteht vor allem aus Insekten, kleinen Spinnen, Würmern und Schnecken. Sein Gesang beginnt etwa eine Stunde vor Sonnenaufgang und ist bis in die Dämmerung fast das ganze Jahr über zu hören. Die Art gilt derzeit als ungefährdet."
---
+++ Lernlisten 7/data.js
@@ -0,0 +1,14 @@
+var lists= [
+    {
+        title:"Vogelarten in Europa",
+
+        cards: [
+            {
+            img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Robin_%289509442456%29.jpg/512px-Robin_%289509442456%29.jpg",
+            text: "Das Rotkehlchen (Erithacus rubecula) ist eine Vogelart aus der Familie der Fliegenschnäpper (Muscicapidae). Es besiedelt Nordafrika, Europa und Kleinasien sowie die Mittelmeerinseln. Seine Nahrung besteht vor allem aus Insekten, kleinen Spinnen, Würmern und Schnecken. Sein Gesang beginnt etwa eine Stunde vor Sonnenaufgang und ist bis in die Dämmerung fast das ganze Jahr über zu hören. Die Art gilt derzeit als ungefährdet."
+            }
+        ]
+    }
+]
+
+export {lists};
